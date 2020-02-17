37154f796c032180dab0a8fa3857c2b0ca329305
Александр Мяснянкин <developlord@gmail.com>
2020-02-17 17:53:34 +0300

    меняет item, main

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
+  if ($().slick) {
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav',
+      dots: true,
+    });
 
+    $('.slider-nav').slick({
+      vertical: true,
+      verticalSwiping: true,
+      slidesToShow: 3,
+      dots: true,
+      autoplay: false,
+      asNavFor: '.slider-for',
+      prevArrow: '<img src="../img/up.png" style="width: 17px; height: 49px; top:100px; position:absolute; cursor:pointer;">',
+      nextArrow: '<img src="../img/down.png" style="width: 17px; height: 49px; bottom: 140px; position:absolute; cursor:pointer;">',
+      responsive: [{
+        breakpoint: 921,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+          vertical: false,
+          prevArrow: '<img src="../img/up.png" style="width: 17px; height: 49px; position:absolute; cursor:pointer; transform:rotate(90deg); bottom:-50px; right:30%; z-index:20;">',
+          nextArrow: '<img src="../img/down.png" style="width: 17px; height: 49px; position:absolute; cursor:pointer; transform:rotate(90deg); bottom:-50px; left:30%; z-index:20;">'
+        }
+      }]
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
   var switchBtns = document.querySelectorAll('.switch__btn');
   var burger = document.querySelector('.burger');
   var menu = document.querySelector('.menu');
@@ -19,35 +52,5 @@ $(document).ready(function () {
     this.classList.toggle('active');
     menu.classList.toggle('active');
     document.body.classList.toggle('noscroll');
-  });
-
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav',
-    dots: true,
-  });
-
-  $('.slider-nav').slick({
-    vertical: true,
-    verticalSwiping: true,
-    slidesToShow: 3,
-    dots: true,
-    autoplay: false,
-    asNavFor: '.slider-for',
-    prevArrow: '<img src="../img/up.png" style="width: 17px; height: 49px; top:100px; position:absolute; cursor:pointer;">',
-    nextArrow: '<img src="../img/down.png" style="width: 17px; height: 49px; bottom: 140px; position:absolute; cursor:pointer;">',
-    responsive: [{
-      breakpoint: 921,
-      settings: {
-        dots: false,
-        slidesToShow: 1,
-        vertical: false,
-        prevArrow: '<img src="../img/up.png" style="width: 17px; height: 49px; position:absolute; cursor:pointer; transform:rotate(90deg); bottom:-50px; right:30%; z-index:20;">',
-        nextArrow: '<img src="../img/down.png" style="width: 17px; height: 49px; position:absolute; cursor:pointer; transform:rotate(90deg); bottom:-50px; left:30%; z-index:20;">'
-      }
-    }]
   });
 });
